d9a241f479e6fb8bb0a9afeb878ca8b411ee586f
David <105919823+Davlany@users.noreply.github.com>
2024-05-15 02:42:38 +0300

    Added case "Бульбозавр" dor testing

--- sha256_test.go
+++ sha256_test.go
@@ -7,8 +7,8 @@ import (
 )
 
 func TestSha256(t *testing.T) {
-	testData := []string{"test string", "test 2 string", "test 3 string"}
-	expectedData := []string{"d5579c46dfcc7f18207013e65b44e4cb4e2c2298f4ac457ba8f82743f31e930b", "608d2dd13cf69bc05ed2f8195f0a8579b7921d74fcc482a5f5e98a27481ab2fe", "1d6c7a8fa66900fc96070be9c0d8837af6d1eca470bb94012e19027800c182e1"}
+	testData := []string{"test string", "test 2 string", "test 3 string", "Бульбозавр"}
+	expectedData := []string{"d5579c46dfcc7f18207013e65b44e4cb4e2c2298f4ac457ba8f82743f31e930b", "608d2dd13cf69bc05ed2f8195f0a8579b7921d74fcc482a5f5e98a27481ab2fe", "1d6c7a8fa66900fc96070be9c0d8837af6d1eca470bb94012e19027800c182e1","c1be68a2e89dbf4f119f5ba03fcf5644925bed75d183d1f81bbab8be492ff5a3"}
 	for i, data := range testData {
 		hashData := sha256.Sum256([]byte(data))
 		if hex.EncodeToString(hashData[:]) == expectedData[i] {
